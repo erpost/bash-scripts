352a39ba2f17382b3670dad2e7237e506e55edb4
Eric Post <25180070+erpost@users.noreply.github.com>
2023-01-11 10:39:50 -0500

    Update arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -8,3 +8,20 @@ for WORD in "${WORDS[@]}";
 do
   echo ${WORD}
 done
+
+
+ANIMAL="dog"
+
+# Checks if ANIMAL is in (list)
+if [[ "$ANIMAL" =~ ^(cat|bird|horse)$ ]]; then
+    echo "$ANIMAL is in the list"
+else
+    echo "$ANIMAL is not in the list"
+fi
+
+# Checks if ANIMAL is in scratch.txt
+if [[ "$ANIMAL" =~ $(echo ^\("$(paste -sd'|' ./scratch.txt)"\)$) ]]; then
+    echo "$ANIMAL is in the list"
+else
+    echo "$ANIMAL is not in the list"
+fi
